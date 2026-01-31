b2940b9ca6c264e2e45c17b701f7225e88e07bf2
HungNQ <196222017+touchmegit1@users.noreply.github.com>
2026-01-31 16:47:39 +0700

    feat: add cloudinary service

---
+++ backend/src/main/java/com/smalltrend/dto/FileUploadResponse.java
@@ -0,0 +1,12 @@
+package com.smalltrend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FileUploadResponse {
+    private String url;
+    private String publicId;
+    private String message;
+}
