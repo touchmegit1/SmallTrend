936de303c93aa2076ce8af0323f8a3d48fc95ce0
Nguyễn Xuân Kiên <xuankienn7@gmail.com>
2026-03-12 19:08:53 +0700

    test(ci): remove DB-dependent SpringBoot context smoke test

--- backend/src/test/java/com/smalltrend/SmallTrendApplicationTests.java
+++ backend/src/test/java/com/smalltrend/SmallTrendApplicationTests.java
@@ -1,9 +1,7 @@
 package com.smalltrend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class SmallTrendApplicationTests {
 
 	@Test
